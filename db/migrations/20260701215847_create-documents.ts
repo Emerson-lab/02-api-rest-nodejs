1e63542398f11f65de5db36338f409c81ac830e1
Emerson-lab <trinddade29@gmail.com>
2026-07-01 18:59:38 -0300

    feat: configure database connection and migrations with knex, update .gitignore and package.json

---
+++ db/migrations/20260701215847_create-documents.ts
@@ -0,0 +1,7 @@
+import type { Knex } from 'knex'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function up(knex: Knex): Promise<void> {}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function down(knex: Knex): Promise<void> {}
